1d38084c10c000884e9c3837f4ab2b651efe00c2
SUTAPAT CHUCHAM <6531503085@lamduan.mfu.ac.th>
2023-09-11 17:23:30 +0700

    commit

--- src/main/java/th/mfu/Concert.java
+++ src/main/java/th/mfu/Concert.java
@@ -4,23 +4,68 @@ import java.util.Date;
 
 public class Concert {
 
-    //TODO: add attributes
+    // TODO: add attributes
+    private int id;
+    private String title;
+    private String performer;
+    private Date date;
+    private String description;
+    
 
     public Concert() {
     }
+
     public Concert(String title, String description) {
-        //TODO: set attributes
-    }
- 
-    public String getTitle() {
-        //TODO: return title
-        return "";
-    }
-    public String getDescription() {
-        //TODO: return description
-        return "";
+        // TODO: set attributes
+        this.title = title;
+        this.description = description;
     }
 
-    //TODO: add getters and setters
+    public String getTitle() {
+        // TODO: return title
+        return title;
+    }
+
+    public String getDescription() {
+        // TODO: return description
+        return description;
+    }
+    // TODO: add getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getPerformer() {
+        return performer;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setPerformer(String performer) {
+        this.performer = performer;
+    }
+
+    
+
+    
+    
 
 }
